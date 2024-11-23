ffef7023267d76159e6319dc01223b1e03b5a633
Tahta001 <tahta.xpplg2@gmail.com>
2024-11-23 10:43:22 +0700

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'book_list_page.dart';
+import 'home_page.dart';
 
 Future<void> main() async {
   await Supabase.initialize(
